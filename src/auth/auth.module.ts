b191c9078b5b8177ee6947cdd5cac32eb2796bcd
csr <122644677+36524760@users.noreply.github.com>
2023-02-10 20:32:17 -0400

    implement config variables in jwtmodule

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,9 +1,9 @@
 import { Module } from "@nestjs/common";
 import { JwtModule } from "@nestjs/jwt/dist";
 import { PassportModule } from "@nestjs/passport/dist";
+import config from "../config/config";
 import { AuthController } from "./auth.controller";
 import { AuthService } from "./auth.service";
-import { jwtContants } from "./constants";
 import { InMemoryUserRepository } from "./inMemory-user-repository";
 import { USER_REPOSITORY } from "./interfaces/user-repository.interface";
 import { JwtStrategy } from "./strategies/jwt.strategy";
@@ -13,9 +13,13 @@ import { LocalStrategy } from "./strategies/local.strategy";
 @Module({
     controllers: [AuthController],
     imports: [PassportModule,
-        JwtModule.register({
-            secret: jwtContants.secret,
-            signOptions: { expiresIn: '20s' }
+        JwtModule.registerAsync({
+            useFactory: async () => ({
+                secret: config().secret,
+                signOptions: {
+                    expiresIn: config().jwt_expires_in
+                }
+            }),
         }),
     ],
     providers: [
