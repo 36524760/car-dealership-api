6bfcd2bbacb825b852671ab46eb4dfe8c4e15522
csr <122644677+36524760@users.noreply.github.com>
2023-02-10 22:01:44 -0400

    update config variables

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,7 +1,7 @@
 
 export default () => ({
     port: parseInt(process.env.PORT, 10) || 3000,
-    secret: process.env.JWT_SECRET,
-    saltOrRounds: parseInt(process.env.HASH_SALT_ROUNDS),
-    jwt_expires_in: process.env.JWT_EXPIRES_IN
+    secret: process.env.JWT_SECRET || 'dsflkjdsfklj',
+    saltOrRounds: parseInt(process.env.HASH_SALT_ROUNDS) || 10,
+    jwt_expires_in: process.env.JWT_EXPIRES_IN || '20s'
 });
