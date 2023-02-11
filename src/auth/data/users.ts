4c1ffcc35bfc809694f9af43cf631bf57349ecdd
csr <122644677+36524760@users.noreply.github.com>
2023-02-10 20:38:41 -0400

    update users data

--- src/auth/data/users.ts
+++ src/auth/data/users.ts
@@ -3,11 +3,11 @@ import { User } from "../interfaces/user.interface";
 
 export const users: User[] = [
     {
-        id: '1a',
-        username: 'user1',
-        password: 'hardpass',
-        roles: [Roles.Admin]
-    },
+        username: 'useradmin1',
+        password: '$2b$10$nJt.WIA4jeNIhtyC7oX33eU23jij.l/Oidl/8zU1StmP5qy.dzjIy',
+        roles: [Roles.Admin],
+        id: 'd'
+      },
     {
         id: '2a',
         username: 'user2',
@@ -20,4 +20,10 @@ export const users: User[] = [
         password: '$2b$10$REIiMeLy4teNpIQj7KsHPuImDU66cqDGU2oqXCBxZW/HL2Tv90sBm',
         roles: [Roles.Admin]
     },
+    {
+        username: 'useradmin202454',
+        password: '$2b$10$qPA37UzoXUsDn7fSZFK2T.DJ8ATCvyDxESfORZmUm87iApfcpT4Wm',
+        roles: [Roles.Admin],
+        id: 'd'
+      }
 ]
